8392216246c3939b169dee50d13d3b2351a7abb8
Bayu Kuncoro Jati <bayukuncorojati46@gmail.com>
2025-09-09 23:28:44 +0700

    feat: passing product with dto collection to products front-page

---
+++ app/Livewire/ProductCatalog.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+use App\Data\ProductData;
+
+class ProductCatalog extends Component
+{
+    public function render()
+    {
+        $result = Product::paginate(9); // ORM / Database Query (Connect to database)
+
+        $products = ProductData::collect($result); // Passing & Processing DTO
+
+        return view('livewire.product-catalog', compact('products')); // Presentation
+    }
+}
